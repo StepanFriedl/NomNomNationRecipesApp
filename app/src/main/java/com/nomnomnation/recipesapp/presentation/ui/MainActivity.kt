6f9faacbccb419d04e2028e5db6df3b691bd8f56
StepanFriedl <stepanfriedl13@gmail.com>
2024-11-07 17:58:20 +0100

    Add Splash screen, add app icon.

--- app/src/main/java/com/nomnomnation/recipesapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/nomnomnation/recipesapp/presentation/ui/MainActivity.kt
@@ -9,15 +9,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nomnomnation.recipesapp.presentation.ui.theme.NomNomNationRecipesAppTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        var isLoading by mutableStateOf(true)
+
+        splashScreen.setKeepOnScreenCondition { isLoading }
         enableEdgeToEdge()
         setContent {
+            LaunchedEffect(Unit) {
+                delay(1500L)
+                isLoading = false
+            }
             NomNomNationRecipesAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
